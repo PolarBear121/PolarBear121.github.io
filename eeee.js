75410d0206739c6fa194c3e4a97bba54e36a9e29
PolarBear121 <37416262+PolarBear121@users.noreply.github.com>
2018-04-05 16:12:44 +0530

    Create eeee.js

---
+++ eeee.js
@@ -0,0 +1,36 @@
+$(document).ready(() => {
+
+    $("#email-input").trigger("focus")
+
+    //handle the focus 
+
+    $("input").focus(function(){
+        console.log(this)
+        $(this).addClass("colored-border")
+
+    });//end focus
+
+     $("input").focusout(function(){
+
+        $(this).removeClass("colored-border")
+
+    });//end focus
+
+    $("form").submit((e)=>{
+
+        e.preventDefault();
+
+        alert("user logged in successfully")
+
+    })
+
+    $("button").click(() =>{
+        alert("Navigation button")
+    })
+
+
+
+
+
+
+}) // end doc ready
